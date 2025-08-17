caf9ec8d5b4dce0ba847d8db7d0bd4ef00cbec04
Ujjwal Sharma <sharmaujjwal2024@gmail.com>
2025-08-18 03:55:40 +0530

    deploying now

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add this line:
+  base: './', 
   plugins: [react(), tailwindcss()]
 })
